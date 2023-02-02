9b9eee92fe2754dda5943c2264f95e342476e68e
fhackett <fhackett.py@gmail.com>
2023-02-01 17:25:39 -0800

    More starting points for tokenizer

--- src/main/scala/com/github/distcompiler/dcal/DCalTokenizer.scala
+++ src/main/scala/com/github/distcompiler/dcal/DCalTokenizer.scala
@@ -1,6 +1,7 @@
 package com.github.distcompiler.dcal
 
 import scala.collection.View
+import scala.util.parsing.combinator.Parsers
 
 object DCalTokenizer {
   final case class Position(fileName: String, line: Int, column: Int)
@@ -18,7 +19,7 @@ object DCalTokenizer {
         case (line, column, Some(char)) => (char, Position(fileName = fileName, line = line, column = column))
       }
 
-  final case class Token(position: Position, data: TokenData)
+  final case class Token(startPosition: Position, endPosition: Position, data: TokenData)
 
   enum TokenData {
     case IntLiteral(value: BigInt)
@@ -26,5 +27,32 @@ object DCalTokenizer {
     case Name(name: String)
   }
 
-  def tokenize(annotatedChars: View[(Char, Position)]): List[Token] = ???
+  object tokenize extends Parsers {
+    override type Elem = (Char,Position)
+
+    def char(ch: Char): Parser[Char] =
+      elem(ch.toString, { case (elemCh, _) => ch == elemCh })
+        .map(_._1)
+
+    def charElem(kind: String)(pred: Char => Boolean): Parser[Char] =
+      elem(kind, { case (elemCh, _) => pred(elemCh) })
+        .map(_._1)
+
+    def withPosition(dataParser: Parser[TokenData]): Parser[Token] =
+      for {
+        (_, startPosition) <- guard(elem("any pair", _ => true))
+        data <- dataParser
+        (_, endPosition) <- guard(elem("any pair", _ => true))
+      } yield Token(startPosition = startPosition, endPosition = endPosition, data = data)
+
+    val intLiteral: Parser[Token] =
+      withPosition {
+        val digit: Parser[Char] = charElem("digit")(ch => ch <= '9' && ch >= '0')
+        rep1(digit).map { digits =>
+          TokenData.StringLiteral(digits.mkString)
+        }
+      }
+
+    def apply(annotatedChars: View[(Char, Position)]): List[Token] = ???
+  }
 }
