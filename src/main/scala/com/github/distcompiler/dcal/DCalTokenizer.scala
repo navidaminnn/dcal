99dcabc07554744ee8c9e4d2c772bd8c79091c58
fhackett <fhackett.py@gmail.com>
2023-02-03 16:57:53 -0800

    Add mini test for tokenizer to show Kim where to start

--- src/main/scala/com/github/distcompiler/dcal/DCalTokenizer.scala
+++ src/main/scala/com/github/distcompiler/dcal/DCalTokenizer.scala
@@ -2,9 +2,17 @@ package com.github.distcompiler.dcal
 
 import scala.collection.View
 import scala.util.parsing.combinator.Parsers
+import scala.util.parsing.input
+import scala.util.parsing.input.{NoPosition, Reader}
 
 object DCalTokenizer {
-  final case class Position(fileName: String, line: Int, column: Int)
+  final case class Position(fileName: String, line: Int, column: Int) {
+    def prev: Position =
+      (line, column) match {
+        case (line, 0) => ???
+        case (line, column) => Position(fileName, line, column - 1)
+      }
+  }
 
   def annotateCharactersWithPosition(chars: View[Char], fileName: String, startLine: Int = 1, startColumn: Int = 1): View[(Char, Position)] =
     chars
@@ -27,6 +35,16 @@ object DCalTokenizer {
     case Name(name: String)
   }
 
+  class CharPosReader(elems: LazyList[(Char,Position)]) extends Reader[(Char,Position)] {
+    override def first: (Char, Position) = elems.head
+
+    override def rest: Reader[(Char, Position)] = new CharPosReader(elems.tail)
+
+    override def atEnd: Boolean = elems.isEmpty
+
+    override def pos: input.Position = NoPosition
+  }
+
   object tokenize extends Parsers {
     override type Elem = (Char,Position)
 
@@ -42,17 +60,26 @@ object DCalTokenizer {
       for {
         (_, startPosition) <- guard(elem("any pair", _ => true))
         data <- dataParser
+        // TODO: this idea is broken. come up with better idea
         (_, endPosition) <- guard(elem("any pair", _ => true))
-      } yield Token(startPosition = startPosition, endPosition = endPosition, data = data)
+      } yield Token(startPosition = startPosition, endPosition = endPosition.prev, data = data)
 
     val intLiteral: Parser[Token] =
       withPosition {
         val digit: Parser[Char] = charElem("digit")(ch => ch <= '9' && ch >= '0')
         rep1(digit).map { digits =>
-          TokenData.StringLiteral(digits.mkString)
+          TokenData.IntLiteral(BigInt.apply(digits.mkString))
         }
       }
 
-    def apply(annotatedChars: View[(Char, Position)]): List[Token] = ???
+    def apply(annotatedChars: View[(Char, Position)]): List[Token] = {
+      val annotatedCharsList = annotatedChars.to(LazyList)
+      // TODO: special-cased to the one test case. add whitespace skipping and other stuff
+      intLiteral.map(List(_)).apply(new CharPosReader(annotatedCharsList)) match {
+        case Success(result, _) => result
+        case Failure(msg, _) => ???
+        case Error(msg, _) => ???
+      }
+    }
   }
 }
--- src/test/scala/com/github/distcompiler/dcal/TestDCalTokenizer.scala
+++ src/test/scala/com/github/distcompiler/dcal/TestDCalTokenizer.scala
@@ -5,35 +5,58 @@ import org.scalatest.funsuite.AnyFunSuite
 class TestDCalTokenizer extends AnyFunSuite {
   import DCalTokenizer.*
 
-  test("annotateCharactersWithPosition simple cases") {
-    def runWithDefaults(str: String): List[(Char,Position)] =
-      annotateCharactersWithPosition(
-        chars = str.view,
-        fileName = "<testfile>",
-      )
-        .toList
+  def pos(line: Int, column: Int): Position =
+    Position(fileName = "<testfile>", line = line, column = column)
 
-    def pos(line: Int, column: Int): Position =
-      Position(fileName = "<testfile>", line = line, column = column)
-
-    assert(runWithDefaults("") == Nil)
-    assert(runWithDefaults("abc") == List(
+  List(
+    "" -> Nil,
+    "abc" -> List(
       ('a', pos(1, 1)),
       ('b', pos(1, 2)),
       ('c', pos(1, 3)),
-    ))
-    assert(runWithDefaults("a\nb") == List(
+    ),
+    "a\nb" -> List(
       ('a', pos(1, 1)),
       ('\n', pos(2, 0)),
       ('b', pos(2, 1)),
-    ))
-    assert(runWithDefaults("a\nb\ncd") == List(
+    ),
+    "a\nb\ncd" -> List(
       ('a', pos(1, 1)),
       ('\n', pos(2, 0)),
       ('b', pos(2, 1)),
       ('\n', pos(3, 0)),
       ('c', pos(3, 1)),
       ('d', pos(3, 2)),
-    ))
+    ),
+  ).foreach {
+    case (input, expectedResult) =>
+      test(s"annotateCharactersWithPosition($input)") {
+        val actualResult = annotateCharactersWithPosition(
+          chars = input.view,
+          fileName = "<testfile>",
+        )
+          .toList
+
+        assert(actualResult == expectedResult)
+      }
+  }
+
+  List(
+    "12 " -> List(
+      Token(
+        startPosition = pos(1, 1),
+        endPosition = pos(1, 2),
+        data = TokenData.IntLiteral(BigInt(12))
+      ),
+    ),
+  ).foreach {
+    case (input, expectedResult) =>
+      test(s"tokenize($input)") {
+        val actualResult = tokenize(annotateCharactersWithPosition(
+          chars = input.view,
+          fileName = "<testfile>",
+        ))
+        assert(actualResult == expectedResult)
+      }
   }
 }
