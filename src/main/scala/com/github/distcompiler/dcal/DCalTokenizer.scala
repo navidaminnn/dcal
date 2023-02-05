93075089d9c347fda9c2d435a312aff38d10193b
fhackett <fhackett.py@gmail.com>
2023-02-04 17:42:51 -0800

    Position handling that actually works

--- src/main/scala/com/github/distcompiler/dcal/DCalTokenizer.scala
+++ src/main/scala/com/github/distcompiler/dcal/DCalTokenizer.scala
@@ -6,26 +6,9 @@ import scala.util.parsing.input
 import scala.util.parsing.input.{NoPosition, Reader}
 
 object DCalTokenizer {
-  final case class Position(fileName: String, line: Int, column: Int) {
-    def prev: Position =
-      (line, column) match {
-        case (line, 0) => ???
-        case (line, column) => Position(fileName, line, column - 1)
-      }
+  final case class Position(fileName: String, line: Int, column: Int) extends input.Position {
+    protected override def lineContents: String = ??? // should be unreachable
   }
-
-  def annotateCharactersWithPosition(chars: View[Char], fileName: String, startLine: Int = 1, startColumn: Int = 1): View[(Char, Position)] =
-    chars
-      .scanLeft((startLine, startColumn - 1, None: Option[Char])) { (acc, char) =>
-        val (prevLine, prevColumn, _) = acc
-        char match {
-          case '\n' => (prevLine + 1, 0, Some('\n'))
-          case char => (prevLine, prevColumn + 1, Some(char))
-        }
-      }
-      .collect {
-        case (line, column, Some(char)) => (char, Position(fileName = fileName, line = line, column = column))
-      }
 
   final case class Token(startPosition: Position, endPosition: Position, data: TokenData)
 
@@ -35,51 +18,99 @@ object DCalTokenizer {
     case Name(name: String)
   }
 
-  class CharPosReader(elems: LazyList[(Char,Position)]) extends Reader[(Char,Position)] {
-    override def first: (Char, Position) = elems.head
+  class CharsReader(fileName: String, elems: LazyList[Char], line: Int, column: Int, val lastPos: Position) extends Reader[Char] {
+    override def first: Char = elems.head
 
-    override def rest: Reader[(Char, Position)] = new CharPosReader(elems.tail)
+    override def rest: Reader[Char] =
+      new CharsReader(
+        fileName = fileName,
+        elems = elems.tail,
+        line = first match {
+          case '\n' => line + 1
+          case _ => line
+        },
+        column = first match {
+          case '\n' => 1
+          case _ => column + 1
+        },
+        lastPos = pos,
+      )
 
     override def atEnd: Boolean = elems.isEmpty
 
-    override def pos: input.Position = NoPosition
+    override def pos: Position =
+      Position(fileName = fileName, line = line, column = column)
   }
 
   object tokenize extends Parsers {
-    override type Elem = (Char,Position)
+    override type Elem = Char
 
     def char(ch: Char): Parser[Char] =
-      elem(ch.toString, { case (elemCh, _) => ch == elemCh })
-        .map(_._1)
+      elem(ch.toString, _ == ch)
 
-    def charElem(kind: String)(pred: Char => Boolean): Parser[Char] =
-      elem(kind, { case (elemCh, _) => pred(elemCh) })
-        .map(_._1)
+    def str(str: String): Parser[String] =
+      str.view
+        .map(char)
+        .reduceOption(_ ~> _)
+        .map(_.map(_ => str))
+        .getOrElse(success(str))
 
     def withPosition(dataParser: Parser[TokenData]): Parser[Token] =
-      for {
-        (_, startPosition) <- guard(elem("any pair", _ => true))
-        data <- dataParser
-        // TODO: this idea is broken. come up with better idea
-        (_, endPosition) <- guard(elem("any pair", _ => true))
-      } yield Token(startPosition = startPosition, endPosition = endPosition.prev, data = data)
+      Parser { input =>
+        // Our special reader implementation has all the info needed to calculate a position in any situation
+        // (even an empty file)
+        val startPosition = input.pos.asInstanceOf[Position]
+        dataParser(input) match {
+          case Success(result, next) =>
+            val endPosition = next.asInstanceOf[CharsReader].lastPos
+            Success(
+              result = Token(startPosition = startPosition, endPosition = endPosition, data = result),
+              next = next,
+            )
+          case noSuccess: NoSuccess => noSuccess
+        }
+      }
 
     val intLiteral: Parser[Token] =
       withPosition {
-        val digit: Parser[Char] = charElem("digit")(ch => ch <= '9' && ch >= '0')
+        val digit: Parser[Char] = elem("digit", ch => ch <= '9' && ch >= '0')
         rep1(digit).map { digits =>
           TokenData.IntLiteral(BigInt.apply(digits.mkString))
         }
       }
 
-    def apply(annotatedChars: View[(Char, Position)]): List[Token] = {
-      val annotatedCharsList = annotatedChars.to(LazyList)
-      // TODO: special-cased to the one test case. add whitespace skipping and other stuff
-      intLiteral.map(List(_)).apply(new CharPosReader(annotatedCharsList)) match {
-        case Success(result, _) => result
-        case Failure(msg, _) => ???
-        case Error(msg, _) => ???
+    val whitespace: Parser[Unit] =
+      (
+        char(' ') | // TODO: support more whitespace than I need for my tests... comments too?
+          char('\n')
+      )
+        .map(_ => ())
+
+    val singleToken: Parser[Option[Token]] =
+      (
+        intLiteral // TODO: rest of the tokens go here with |
+        ).map(Some(_)) |
+        whitespace.map(_ => None)
+
+    def apply(chars: IterableOnce[Char], startLine: Int = 1, startColumn: Int = 1, fileName: String): Iterator[Token] =
+      Iterator.unfold(new CharsReader(
+        fileName = fileName,
+        elems = chars.iterator.to(LazyList),
+        line = startLine,
+        column = startColumn,
+        lastPos = Position(fileName = fileName, line = startLine, column = startColumn),
+      )) { reader =>
+        if(reader.atEnd) {
+          None
+        } else {
+          singleToken.apply(reader) match {
+            case Success(tokenOpt, next) =>
+              Some((tokenOpt, next.asInstanceOf[CharsReader]))
+            case Failure(msg, _) => ??? // throw exception probably
+            case Error(msg, _) => ??? // same
+          }
+        }
       }
-    }
+        .flatten
   }
 }
--- src/test/scala/com/github/distcompiler/dcal/TestDCalTokenizer.scala
+++ src/test/scala/com/github/distcompiler/dcal/TestDCalTokenizer.scala
@@ -9,39 +9,13 @@ class TestDCalTokenizer extends AnyFunSuite {
     Position(fileName = "<testfile>", line = line, column = column)
 
   List(
-    "" -> Nil,
-    "abc" -> List(
-      ('a', pos(1, 1)),
-      ('b', pos(1, 2)),
-      ('c', pos(1, 3)),
+    "12" -> List(
+      Token(
+        startPosition = pos(1, 1),
+        endPosition = pos(1, 2),
+        data = TokenData.IntLiteral(BigInt(12))
+      ),
     ),
-    "a\nb" -> List(
-      ('a', pos(1, 1)),
-      ('\n', pos(2, 0)),
-      ('b', pos(2, 1)),
-    ),
-    "a\nb\ncd" -> List(
-      ('a', pos(1, 1)),
-      ('\n', pos(2, 0)),
-      ('b', pos(2, 1)),
-      ('\n', pos(3, 0)),
-      ('c', pos(3, 1)),
-      ('d', pos(3, 2)),
-    ),
-  ).foreach {
-    case (input, expectedResult) =>
-      test(s"annotateCharactersWithPosition($input)") {
-        val actualResult = annotateCharactersWithPosition(
-          chars = input.view,
-          fileName = "<testfile>",
-        )
-          .toList
-
-        assert(actualResult == expectedResult)
-      }
-  }
-
-  List(
     "12 " -> List(
       Token(
         startPosition = pos(1, 1),
@@ -49,13 +23,26 @@ class TestDCalTokenizer extends AnyFunSuite {
         data = TokenData.IntLiteral(BigInt(12))
       ),
     ),
+    "12\n13" -> List(
+      Token(
+        startPosition = pos(1, 1),
+        endPosition = pos(1, 2),
+        data = TokenData.IntLiteral(BigInt(12))
+      ),
+      Token(
+        startPosition = pos(2, 1),
+        endPosition = pos(2, 2),
+        data = TokenData.IntLiteral(BigInt(13))
+      ),
+    ),
   ).foreach {
     case (input, expectedResult) =>
       test(s"tokenize($input)") {
-        val actualResult = tokenize(annotateCharactersWithPosition(
-          chars = input.view,
+        val actualResult = tokenize(
+          chars = input,
           fileName = "<testfile>",
-        ))
+        )
+          .toList
         assert(actualResult == expectedResult)
       }
   }
