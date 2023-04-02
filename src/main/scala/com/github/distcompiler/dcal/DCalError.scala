890298cb0c60ddc7a44ccc73106d4378fa009d31
Kim Dao <kdaokdao@gmail.com>
2023-04-02 00:46:06 -0700

    Detailed error classes

--- src/main/scala/com/github/distcompiler/dcal/DCalError.scala
+++ src/main/scala/com/github/distcompiler/dcal/DCalError.scala
@@ -6,23 +6,47 @@ abstract class DCalError {
 
 // Use final case class here because
 // 1- final: prevents extends, 2- case: enables pattern matching, 3- class: creates instances
+
+/**
+ * Thrown when the DCal file of an imported module is not found in the file system.
+ */
 final case class ModuleNotFound(dcalModule: DCalAST.Module) extends DCalError {
   val description = s"Referenced module ${dcalModule.name} not found: <line number>"
 }
+
+/**
+ * Thrown when a definition name in a procedure call is not found.
+ */
 final case class DefinitionNotFound(dcalDef: DCalAST.Definition) extends DCalError {
   val description = s"Referenced definition ${dcalDef.name} not found: <line number>"
 }
+
+/**
+ * Thrown when a name, in a position other than an import statement or a procedure call, is not found.
+ */
 final case class NameNotFound(name: String) extends DCalError {
   val description = s"Referenced name $name not found: <line number>"
 }
+
+/**
+ * Thrown when memberName in <\name>.<\memberName> is not found.
+ */
 final case class MemberNotFound(name: String, memberName: String) extends DCalError {
   val description = s"$memberName not a member of $name: <line number>"
 }
 
-final case class RedefinedName(name: String) extends DCalError {
+/**
+ * Thrown when an existing name is redeclared. Any one of a definition, a let statement, or a var statement can cause
+ * a name redeclaration error, regardless of what the initial declaration is.
+ */
+final case class RedeclaredName(name: String) extends DCalError {
   val description = s"$name already defined in this scope: <line number>"
 }
 
+/**
+ * Thrown when a name other than var name appears in an assign pair's left-hand side. This includes module, definition,
+ * and value names.
+ */
 final case class ReassignmentToImmutable(name: String) extends DCalError {
-  val description = s"immutable $name reassigned: <line number>"
+  val description = s"Immutable $name reassigned: <line number>"
 }
