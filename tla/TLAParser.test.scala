0e9e92c41c9ce7a260ae9d7bc97ad4a0b49ae055
fhackett <fhackett.py@gmail.com>
2025-01-29 13:44:49 -0800

    revert mistakenly committed DAP activation

--- tla/TLAParser.test.scala
+++ tla/TLAParser.test.scala
@@ -29,11 +29,12 @@ class TLAParserTests extends munit.FunSuite, test.WithTLACorpus:
     assume(!top.hasErrors, top)
 
     TLAParser(
-      top,
-      tracer = DebugAdapter(
-        "localhost",
-        4711
-      ) // , tracer = Manip.RewriteDebugTracer(os.pwd / "dbg_passes")
+      top
+      // tracer = DebugAdapter(
+      //   "localhost",
+      //   4711
+      // )
+      // , tracer = Manip.RewriteDebugTracer(os.pwd / "dbg_passes")
     )
 
     // re-enable if interesting:
